d302db0de35af9967d35cb90d96a4218a512a843
erics3n <ericsenflp@gmail.com>
2018-10-26 12:03:44 -0300

    WinForms e WebForms

--- Solucao/01_console/Program.cs
+++ Solucao/01_console/Program.cs
@@ -8,9 +8,10 @@ namespace _01_console
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)       //ponto de partida da exeçução do programa.
         {
-            //
+            Console.Write("Olá Mundo!");     //(Console) - nada mais é do que acessar o prompt de comando//write - comando para escrever na tela//writeline - escreve e pula uma linha
+            Console.ReadKey();              //faz com que o console leia uma tecla, vai esperar com que o usuario informe uma tecla no PC
         }
     }
 }
